26bf1c2c0d62076a2679506263ca5a10bbed8ea9
rotciv0101 <victorgabriel1730@gmail.com>
2021-08-30 12:37:54 -0300

    revisão - variáveis e estrutura de decisão

---
+++ aplicacao_dart/bin/exemplo_variaveis.dart
@@ -0,0 +1,17 @@
+import 'package:aplicacao_dart/aplicacao_dart.dart' as aplicacao_dart;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Informe o nome da sua cidade: ');
+  var cidade = stdin.readLineSync();
+
+  print('Informe o seu endereço (Rua e Bairro): ');
+  var endereco = stdin.readLineSync();
+
+  print('Informe o número da sua casa: ');
+  var entradaNCasa = stdin.readLineSync();
+  var numeroCasa = int.parse(entradaNCasa!);
+
+  
+  print('Cidade: $cidade\n Endereço: $endereco\n Número da casa: $numeroCasa\n');
+}
